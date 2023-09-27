77621def3cc116dd8fc1a4333573afd4e26fa8bc
Jason Siders <41714875+jasonsiders@users.noreply.github.com>
2023-09-27 13:15:26 -0500

    replaced cpe component

--- force-app/main/default/lwc/loggerCpe/loggerCpe.js
+++ force-app/main/default/lwc/loggerCpe/loggerCpe.js
@@ -1,13 +1,8 @@
-import { api } from "lwc";
-import {
-	CustomPropertyEditor,
-	DataTypes,
-	Error,
-	ValueChangedEvent,
-} from "c/customPropertyEditor";
+import { LightningElement, api } from "lwc";
 
-// TODO: Replace this with a real description!
-const DESCRIPTION = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+// TODO: Add real copy
+const DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+// Maps to the expected @InvocableVariable names from the LogInput class
 const INVOCABLE_VARS = {
 	BODY: "body",
 	LEVEL: "level",
@@ -15,6 +10,7 @@ const INVOCABLE_VARS = {
 	RELATED_RECORD: "relatedRecordId",
 	SOURCE: "source"
 };
+// Maps to valid System.LoggingLevel enum options
 const LEVEL_OPTIONS = [
 	{ label: "FINEST", value: "FINEST" },
 	{ label: "FINER", value: "FINER" },
@@ -25,18 +21,21 @@ const LEVEL_OPTIONS = [
 	{ label: "ERROR", value: "ERROR" }
 ];
 
-export default class CpeLogger extends CustomPropertyEditor {
+export default class LoggerCpe extends LightningElement {
+	@api inputVariables;
 	description = DESCRIPTION;
 	invocableVars = INVOCABLE_VARS;
 	levelOptions = LEVEL_OPTIONS;
 
 	@api validate() {
 		let errors = [];
-		const innerComponents = ["lightning-combobox", "lightning-textarea"];
+		const innerComponents = ["lightning-combobox", "lightning-input", "lightning-textarea"];
 		innerComponents?.forEach((componentName) => {
 			const component = this.template.querySelector(componentName);
 			if (!component?.reportValidity()) {
-				errors.push(new Error(component?.label, `${component?.label} is invalid`));
+				const label = component?.label;
+				const error = { key: label, errorString: `${label} is invalid`};
+				errors?.push(error);
 			}
 		});
 		return errors;
@@ -62,10 +61,25 @@ export default class CpeLogger extends CustomPropertyEditor {
 		return this.getInvocableVariable(this.invocableVars?.SOURCE)?.value;
 	}
 
+	getInvocableVariable(name) {
+		// Retrieve the named Invocable Variable
+		return this.inputVariables?.find((variable) => variable?.name === name);
+	} 
+
 	handleChange(event) {
+		// Publish a ValueChangedEvent to send the input value to matching inputVariable
 		const elementName = event?.target?.name || event?.detail?.name;
 		const newValue = event?.detail?.newValue || event?.detail?.value;
-		const valueChangedEvent = new ValueChangedEvent(elementName, newValue, DataTypes.STRING);
+		const valueChangedEvent = new CustomEvent("configuration_editor_input_value_changed", {
+			bubbles: true,
+			cancelable: false,
+			composed: true, 
+			detail: {
+				name: elementName,
+				newValue,
+				newValueDataType: "String"
+			}
+		});
 		this.dispatchEvent(valueChangedEvent);
 	}
 }
