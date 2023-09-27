1b71cda02f5c769f6545d1f7d484f1188a640df7
Jason Siders <41714875+jasonsiders@users.noreply.github.com>
2023-09-27 13:36:48 -0500

    Feature/invocable logger (#10)
    * created initial version of InvocableLogger
    
    * created empty test
    
    * added tests for invocable logger
    
    * made log method private
    
    * created cpe components
    
    * created invocablelogpublisher class
    
    * reworked record id to handle null values
    
    * added source field
    
    * renamed "Apex Class" to "Logged From"
    
    * modified overload methods & tests
    
    * created LogTestUtils
    
    * implemented LogTestUtils
    
    * implemented LogInput in LwcLogger
    
    * implemented LogInput in InvocableLogger
    
    * added note to LwcLogger class
    
    * cleaned up assert
    
    * reordered "Logged From" in page layout
    
    * moved loggerCpe invocable variables into object
    
    * added loggedFrom field
    
    * added related record Id
    
    * checkpoint: experimenting w/lightning layout
    
    * attempting to move away from cpeCombobox
    
    * removed cpeCombobox - wasn't adding value
    
    * fixed layout styling
    
    * added space for description (with mock copy for now)
    
    * fix misnamed test
    
    * replaced cpe component
    
    * removed description - reading docs aren't so bad
    
    * added LwcLogger to permission set

---
+++ force-app/main/default/lwc/loggerCpe/loggerCpe.js
@@ -0,0 +1,82 @@
+import { LightningElement, api } from "lwc";
+
+// Maps to @InvocableVariable names from the LogInput class
+const INVOCABLE_VARS = {
+	BODY: "body",
+	LEVEL: "level",
+	LOGGED_FROM: "loggedFrom",
+	RELATED_RECORD: "relatedRecordId",
+	SOURCE: "source"
+};
+// Maps to valid System.LoggingLevel enum options
+const LEVEL_OPTIONS = [
+	{ label: "FINEST", value: "FINEST" },
+	{ label: "FINER", value: "FINER" },
+	{ label: "FINE", value: "FINE" },
+	{ label: "INFO", value: "INFO" },
+	{ label: "DEBUG", value: "DEBUG" },
+	{ label: "WARN", value: "WARN" },
+	{ label: "ERROR", value: "ERROR" }
+];
+
+export default class LoggerCpe extends LightningElement {
+	@api inputVariables;
+	invocableVars = INVOCABLE_VARS;
+	levelOptions = LEVEL_OPTIONS;
+
+	@api validate() {
+		let errors = [];
+		const innerComponents = ["lightning-combobox", "lightning-input", "lightning-textarea"];
+		innerComponents?.forEach((componentName) => {
+			const component = this.template.querySelector(componentName);
+			if (!component?.reportValidity()) {
+				const label = component?.label;
+				const error = { key: label, errorString: `${label} is invalid`};
+				errors?.push(error);
+			}
+		});
+		return errors;
+	}
+
+	get body() {
+		return this.getInvocableVariable(this.invocableVars?.BODY)?.value;
+	}
+
+	get level() {
+		return this.getInvocableVariable(this.invocableVars?.LEVEL)?.value;
+	}
+
+	get loggedFrom() {
+		return this.getInvocableVariable(this.invocableVars?.LOGGED_FROM)?.value;
+	}
+
+	get relatedRecordId() {
+		return this.getInvocableVariable(this.invocableVars?.RELATED_RECORD)?.value;
+	}
+
+	get source() {
+		return this.getInvocableVariable(this.invocableVars?.SOURCE)?.value;
+	}
+
+	getInvocableVariable(name) {
+		// Retrieve the named Invocable Variable
+		return this.inputVariables?.find((variable) => variable?.name === name);
+	} 
+
+	handleChange(event) {
+		// Publish a ValueChangedEvent to send the input value to matching inputVariable
+		const elementName = event?.target?.name || event?.detail?.name;
+		const newValue = event?.detail?.newValue || event?.detail?.value;
+		const valueChangedEvent = new CustomEvent("configuration_editor_input_value_changed", {
+			bubbles: true,
+			cancelable: false,
+			composed: true, 
+			detail: {
+				name: elementName,
+				newValue,
+				newValueDataType: "String"
+			}
+		});
+		this.dispatchEvent(valueChangedEvent);
+	}
+}
